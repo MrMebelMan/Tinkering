2f8bff2749e76a98cd0b2565eff2bb2d62cde972
Vladyslav Burzakovskyy <burzakovskij@gmail.com>
2017-12-15 22:36:19 +0100

    Update for_byte.c
    added comparison with big-endian

--- for_byte.c
+++ for_byte.c
@@ -9,6 +9,7 @@ int main ( void )
   uint32_t num = 0x01ABCDEF;
 
   /* prints 0xEF 0xCD 0xAB 0x01 (on little-endian) */
+  /* and    0x01 0xAB 0xCD 0xEF (on big-endian) */
   for ( byte = (uint8_t*) &num; byte != (uint8_t*) &num + sizeof (num); byte++ )
     printf ( "0x%.2X ", *byte );
   puts ( "" );
